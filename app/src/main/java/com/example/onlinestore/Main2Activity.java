bf92368be03a57ae999f5bd8be59fc8774ea7c73
Online Grocery <testgithubv@gmail.com>
2022-07-06 18:40:39 +0530

    HomeFragment code with xml file, models and adapters

--- app/src/main/java/com/example/onlinestore/Main2Activity.java
+++ app/src/main/java/com/example/onlinestore/Main2Activity.java
@@ -1,12 +1,12 @@
 package com.example.onlinestore;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,26 +16,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.onlinestore.databinding.ActivityMain2Binding;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity  {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
 
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        //    private ActivityMain2Binding binding;
+        com.example.onlinestore.databinding.ActivityMain2Binding binding = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain2.toolbar);
-        binding.appBarMain2.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        binding.appBarMain2.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -50,7 +46,7 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main2, menu);
 
--- app/src/main/java/com/example/onlinestore/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/onlinestore/ui/home/HomeFragment.java
@@ -4,34 +4,146 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.onlinestore.databinding.FragmentHomeBinding;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class HomeFragment extends Fragment {
+import com.example.onlinestore.R;
+import com.example.onlinestore.adapters.HomeAdapter;
+import com.example.onlinestore.adapters.PopularAdapters;
+import com.example.onlinestore.adapters.RecommendedAdapter;
 
-    private FragmentHomeBinding binding;
+import com.example.onlinestore.models.HomeCategory;
+import com.example.onlinestore.models.PopularModel;
+import com.example.onlinestore.models.RecommendedModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends  Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    ScrollView scrollView;
+    ProgressBar progressBar;
+    RecyclerView popularRec,homeCatRec,recommendedRec;
+    FirebaseFirestore db;
+
+    List<PopularModel> popularModelList;
+    PopularAdapters popularAdapters;
+
+    //Home Category
+    List<HomeCategory> categoryList;
+    HomeAdapter homeAdapter;
+
+    //Recommended
+    List<RecommendedModel> recommendedModelList;
+    RecommendedAdapter recommendedAdapter;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        super.onCreate(savedInstanceState);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        View root=inflater.inflate(R.layout.fragment_home,container,false);
+        db= FirebaseFirestore.getInstance();
+        popularRec=root.findViewById(R.id.poprec);
+        homeCatRec=root.findViewById(R.id.explore_rec);
+        //recommendedRec=root.findViewById(R.id.recommended_rec);
+        scrollView=root.findViewById(R.id.scroll_view);
+       // progressBar=root.findViewById(R.id.progressbar);
+
+        //progressBar.setVisibility(View.VISIBLE);
+        scrollView.setVisibility(View.GONE);
+        //popular items
+
+        popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        popularModelList=new ArrayList<>();
+        popularAdapters= new PopularAdapters(getActivity(),popularModelList);
+        popularRec.setAdapter(popularAdapters);
+        db.collection("PopularProducts")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularModel popularModel=document.toObject(PopularModel.class);
+                                popularModelList.add(popularModel);
+                                popularAdapters.notifyDataSetChanged();
+
+                               // progressBar.setVisibility(View.GONE);
+                                scrollView.setVisibility(View.VISIBLE);
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        //Home Category
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryList=new ArrayList<>();
+        homeAdapter= new HomeAdapter(getActivity(),categoryList);
+        homeCatRec.setAdapter(homeAdapter);
+        db.collection("HomeCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                HomeCategory homeCategory=document.toObject(HomeCategory.class);
+                                categoryList.add(homeCategory);
+                                homeAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        //Recommended items
+//        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+//        recommendedModelList=new ArrayList<>();
+//        recommendedAdapter= new RecommendedAdapter(getActivity(),recommendedModelList);
+//        recommendedRec.setAdapter(recommendedAdapter);
+//        db.collection("Recommended")
+//                .get()
+//                .addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//                            RecommendedModel recommendedModel=document.toObject(RecommendedModel.class);
+//                            recommendedModelList.add(recommendedModel);
+//                            recommendedAdapter.notifyDataSetChanged();
+//                        }
+//                    } else {
+//                        Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
         return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        com.example.onlinestore.databinding.FragmentHomeBinding binding = null;
     }
 }
